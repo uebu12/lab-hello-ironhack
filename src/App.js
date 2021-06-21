c73441360eb6071344883105c13dcfd4b9903744
John Doe <gabrifurquim@gmail.com>
2021-06-21 20:55:19 -0300

    not done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ironhackLogo from "./assets/images/ironhack-logo.svg"
+import menuLogo from "./assets/images/menu-top.svg"
+import icon1 from "./assets/images/icon1.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={ironhackLogo}/>
+        <img src={menuLogo}/>
+        <div>
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn a Frontend framework from scratch, to become a Ninka Developer</p>
+          <span>Awesome!</span>
+        </div>
       </header>
+      <section>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   );
 }

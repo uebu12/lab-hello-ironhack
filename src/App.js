b7ddf978381922f1eb6846278739d0a31fcd2dc3
John Doe <gabrifurquim@gmail.com>
2021-06-21 21:17:33 -0300

    not done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import ironhackLogo from "./assets/images/ironhack-logo.svg"
 import menuLogo from "./assets/images/menu-top.svg"
 import icon1 from "./assets/images/icon1.png"
+import icon2 from "./assets/images/icon2.png"
+import icon3 from "./assets/images/icon3.png"
+import icon4 from "./assets/images/icon4.png"
 
 function App() {
   return (
@@ -18,16 +21,24 @@ function App() {
       </header>
       <section>
         <div>
-
+          <img src={icon1}/>
+          <h3>Declarative</h3>
+          <p>React makes it painless to create interactives UIs.</p>
         </div>
         <div>
-
+        <img src={icon2}/>
+        <h3>Components</h3>
+          <p>Build encapsulated components that manage their state.</p>
         </div>
         <div>
-
+        <img src={icon3}/>
+        <h3>Single-Way</h3>
+          <p>A set of immutable values are passed to the component's.</p>
         </div>
         <div>
-
+        <img src={icon4}/>
+        <h3>JSX</h3>
+          <p>Statically-typed, designed to run on modern browsers.</p>
         </div>
       </section>
     </div>

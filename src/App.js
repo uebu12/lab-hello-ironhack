0c978a8c50d3bcc19645a3c190a7a83770a20be4
John Doe <gabrifurquim@gmail.com>
2021-06-21 22:34:59 -0300

    not done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-
-import './App.css';
-import ironhackLogo from "./assets/images/ironhack-logo.svg"
-import menuLogo from "./assets/images/menu-top.svg"
-import icon1 from "./assets/images/icon1.png"
-import icon2 from "./assets/images/icon2.png"
-import icon3 from "./assets/images/icon3.png"
-import icon4 from "./assets/images/icon4.png"
+import "./App.css";
+import ironhackLogo from "./assets/images/ironhack-logo.svg";
+import menuLogo from "./assets/images/menu-top.svg";
+import icon1 from "./assets/images/icon1.png";
+import icon2 from "./assets/images/icon2.png";
+import icon3 from "./assets/images/icon3.png";
+import icon4 from "./assets/images/icon4.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ironhackLogo}/>
-        <img src={menuLogo}/>
+        <nav>
+          <img src={ironhackLogo} alt="ironhack-logo" />
+          <img src={menuLogo} alt="" />
+        </nav>
         <div>
           <h1>Say hello to ReactJS</h1>
-          <p>You will learn a Frontend framework from scratch, to become a Ninka Developer</p>
+          <p>
+            You will learn a Frontend framework from scratch, to become a Ninka
+            Developer.
+          </p>
           <span>Awesome!</span>
         </div>
       </header>
-      <section>
-        <div>
-          <img src={icon1}/>
+      <section className="Second-half">
+        <div className="Second-half-divs">
+          <img src={icon1} alt="Declarative" />
           <h3>Declarative</h3>
           <p>React makes it painless to create interactives UIs.</p>
         </div>
-        <div>
-        <img src={icon2}/>
-        <h3>Components</h3>
+        <div className="Second-half-divs">
+          <img src={icon2} alt="Components" />
+          <h3>Components</h3>
           <p>Build encapsulated components that manage their state.</p>
         </div>
-        <div>
-        <img src={icon3}/>
-        <h3>Single-Way</h3>
+        <div className="Second-half-divs">
+          <img src={icon3} alt="Single-Way" />
+          <h3>Single-Way</h3>
           <p>A set of immutable values are passed to the component's.</p>
         </div>
-        <div>
-        <img src={icon4}/>
-        <h3>JSX</h3>
+        <div className="Second-half-divs">
+          <img src={icon4} alt="JSX" />
+          <h3>JSX</h3>
           <p>Statically-typed, designed to run on modern browsers.</p>
         </div>
       </section>
